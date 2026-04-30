c391a7fe5cbf46f9db1a2041d903c183fbb3f695
Mark Mayes <20658057+mm-dev-rocks@users.noreply.github.com>
2026-04-30 20:31:51 +0100

    Move practice file out of root dir

---
+++ 000-ASSETS/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char *src = "GET /path HTTP/1.1\r\n";
+
+    if (src == NULL) {
+        return -1;
+    }
+    if (*src == '\0') {
+        return -1;
+    }
+
+    int srcIndex = 0;
+    int tokensCount = 0;
+    char tokens[3][128];
+
+    while (src[srcIndex] != '\0') {
+        printf("while 1, src: %c\n", src[srcIndex]);
+        int destIndex = 0;
+        while (src[srcIndex] != ' ') {
+            printf("while 2, src: %c\n", src[srcIndex]);
+            if (src[srcIndex] == '\n') {
+                if (tokensCount == 2) {
+                    for (int i = 0; i < 3; i++) {
+                        printf("%s\n", tokens[i]);
+                    }
+                    printf("point2\n");
+                    return 0;
+                } else {
+                    // invalid
+                    printf("error\n");
+                    return -1;
+                }
+            }
+            tokens[tokensCount][destIndex] = src[srcIndex];
+            srcIndex++;
+            destIndex++;
+        }
+        // Null terminate token
+        tokens[tokensCount][destIndex] = '\0';
+        // Go past space
+        srcIndex++;
+        // Go to next token
+        tokensCount++;
+    }
+    tokens[tokensCount][srcIndex] = '\0';
+
+    for (int i = 0; i < 3; i++) {
+        printf("%s\n", tokens[i]);
+    }
+    printf("point2\n");
+    return 0;
+}
